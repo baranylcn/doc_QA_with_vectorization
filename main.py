56c8e4aac908bf5e56497eeaa0c791a3df1be6e8
Baran Yalçın <98966968+baranylcn@users.noreply.github.com>
2024-04-17 14:22:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,13 +4,17 @@ from langchain.indexes import VectorstoreIndexCreator
 
 os.environ["OPENAI_API_KEY"] = "OPENAI_API_KEY"
 
-def query_with_doc(doc_path, query):
+def vectorization(doc_path):
     text_loader = TextLoader(doc_path, encoding='utf-8')
     index = VectorstoreIndexCreator().from_loaders([text_loader])
+    return index
+
+def query_with_doc(index, query)
     result = index.query_with_sources(query)
     return result["answer"]
 
 # Usage
 doc_path = "DOC_FILE_PATH"
 query = "QUESTION"
-query_with_doc(doc_path, query)
+index = vectorization(doc_path)
+print(query_with_doc(index, query))
